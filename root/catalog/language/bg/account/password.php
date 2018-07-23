59453384462b6d3a978e098d3d3b93752de03070
Suzi Hatim <suzy.hatim@gmail.com>
2018-07-23 12:06:31 +0300

    Some translations are changed, mainly in address pages

--- root/catalog/language/bg/account/password.php
+++ root/catalog/language/bg/account/password.php
@@ -8,17 +8,17 @@
  *      Bitcoin: 1P3Zk93fhKURWT1rXiYxiLGuJ59oETxuTi
  */
 // Heading
-$_['heading_title']  = 'Промяна на парола';
+$_['heading_title']  = 'Смяна на парола';
 
 // Text
 $_['text_account']   = 'Профил';
-$_['text_password']  = 'Вашата парола';
-$_['text_success']   = 'Готово, паролата ви бе обновена успешно.';
+$_['text_password']  = 'Нова парола';
+$_['text_success']   = 'Готово, паролата ви бе променена успешно.';
 
 // Entry
 $_['entry_password'] = 'Парола';
-$_['entry_confirm']  = 'Паролата повторно:';
+$_['entry_confirm']  = 'Парола потвърждение';
 
 // Error
 $_['error_password'] = 'Паролата трябва да е между 4 и 20 символа!';
-$_['error_confirm']  = 'Паролата в двете полета не е една и съща!';
+$_['error_confirm']  = 'Паролата в двете полета не съвпада!';
